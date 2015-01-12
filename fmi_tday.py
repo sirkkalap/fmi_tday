1761e1476e5881d11e3239f0130b3c209c397ba2
Petri Sirkkala <sirpete@iki.fi>
2015-01-13 00:39:52 +0200

    Place info

--- fmi_tday.py
+++ fmi_tday.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding=utf-8
+
 import sys
+import io
 import urllib2
 from lxml import etree
 
@@ -22,6 +24,13 @@ xmlurl = urllib2.urlopen(url)
 
 dom = etree.parse(xmlurl)
 
+#f = io.open('tvp.xml', 'w')
+#f.write(etree.tounicode(dom, pretty_print=True))
+#f.close()
+
+locations = dom.xpath('//target:Location', namespaces={'target': 'http://xml.fmi.fi/namespace/om/atmosphericfeatures/0.95'})
+gmlnames = [l.findall('./{http://www.opengis.net/gml/3.2}name') for l in locations]
+print [n.text for n in gmlnames[0]]
 measurementTVPs = dom.xpath('//wml2:point/wml2:MeasurementTVP', namespaces={'wml2': 'http://www.opengis.net/waterml/2.0'})
 temps = [(m.getchildren()[0].text, m.getchildren()[1].text) for m in measurementTVPs]
 
